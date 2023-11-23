4d75c78fa452564eb5fc6dd6d035d0faa4a2e649
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2023-11-23 19:39:06 +0700

    Controller CreateTodo

---
+++ controller/create_todo.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	"github.com/eby8zevin/golang-todos/model"
+	"github.com/labstack/echo"
+)
+
+func CreateTodoController(e *echo.Echo, db *sql.DB) {
+	e.POST("/create_todos",func(ctx echo.Context) error {
+		var request model.CreateRequest
+		json.NewDecoder(ctx.Request().Body).Decode(&request)
+
+		_, err := db.Exec(
+			"INSERT INTO todos (title, description, done) VALUES (?, ?, 0)",
+			request.Title,
+			request.Description,
+		)
+		
+		if err != nil {
+			return ctx.String(http.StatusInternalServerError, err.Error())
+		}
+
+		responseData := model.DefaultResponse{
+			Success: true,
+			Message: "Data Added successfully",
+		}
+
+		return ctx.JSON(http.StatusOK, responseData)
+	})
+}
