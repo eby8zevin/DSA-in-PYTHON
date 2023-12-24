137b62be9e09c1fffb233f68b5aec867e15b3494
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2023-12-24 11:03:27 +0700

    Added middleware and controller

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/eby8zevin/golang-todos/controller"
 	"github.com/eby8zevin/golang-todos/database"
+	"github.com/eby8zevin/golang-todos/middleware"
 	"github.com/labstack/echo"
 )
 
@@ -17,11 +18,19 @@ func main() {
 
 	e := echo.New()
 
+	e.Use(middleware.AuthMiddleware)
+
 	controller.GetAllTodosController(e, db)
 	controller.CreateTodoController(e, db)
 	controller.DeleteTodoController(e, db)
 	controller.UpdateTodoController(e, db)
 	controller.CheckTodoController(e, db)
+
+	controller.RegisterUserController(e, db)
+	controller.LoginUserController(e, db)
+	controller.CreateScopeController(e, db)
+	controller.DeleteScopeController(e, db)
+	controller.AssignScopeController(e, db)
 
 	e.Start(":8080")
 }
